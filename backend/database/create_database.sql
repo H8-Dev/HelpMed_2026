bfe7a89e9d4d338f02a3290823edc4694f9b114a
H8-Dev <h8.pr0j1@gmail.com>
2026-06-19 12:21:07 -0300

    Rodando

---
+++ backend/database/create_database.sql
@@ -0,0 +1,38 @@
+create database helpmed;
+use helpmed;
+
+create table pacientes(
+CPF varchar(14) primary key not null,
+senha varchar(50) not null,
+nome varchar(50) not null,
+sobrenome varchar(80) not null,
+data_create timestamp default current_timestamp not null
+) engine = InnoDB;
+
+create table medicos(
+CRM varchar(15) primary key not null,
+CPF varchar(14) not null,
+senha varchar(50) not null,
+nome varchar(50) not null,
+sobrenome varchar(80) not null,
+data_create timestamp default current_timestamp not null
+) engine = InnoDB;
+
+create table chat(
+chat_id int primary key not null,
+url varchar(2083) not null,
+last_update timestamp default current_timestamp not null
+) engine = InnoDB;
+
+create table arquivos(
+arq_id int primary key not null,
+type varchar(20) not null,
+url varchar(2083) not null,
+last_update timestamp default current_timestamp not null
+) engine = InnoDB;
+
+create table pagamentos(
+pag_id int primary key not null,
+total decimal not null,
+data_hora timestamp default current_timestamp not null
+) engine = InnoDB;
