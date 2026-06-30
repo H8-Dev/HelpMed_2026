cd9344bfc2d6aa1f200abf9c8da3681566248c99
H8-Dev <h8.pr0j1@gmail.com>
2026-06-30 19:00:46 -0300

    Controller do médico - Starting stages

--- backend/controllers/medico_controller.py
+++ backend/controllers/medico_controller.py
@@ -1,1 +1,5 @@
-from flask import Flask, jsonify, render_template, request, url_for, redirect
+from flask import Flask, jsonify, render_template, request, url_for, redirect, Blueprint
+
+from models.database import db
+
+med_controller = Blueprint("med_controller", __name__)
