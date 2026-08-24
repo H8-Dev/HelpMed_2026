b35a8fdcdf502678e6acb85908ae9c97575b39ab
H8-Dev <h8.pr0j1@gmail.com>
2026-08-24 17:13:49 -0300

    Renamed properly and added logos for all new site pages

--- backend/controllers/atend_previo_controller.py
+++ backend/controllers/atend_previo_controller.py
@@ -1,1 +1,10 @@
 from flask import jsonify, request, Blueprint #type: ignore
+from models.database import db
+
+
+prev_controller = Blueprint("prev_controller", __name__)
+
+class AtendPrevioController:
+
+
+    pass
