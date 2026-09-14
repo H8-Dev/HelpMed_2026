41a853a7148df64b73c3e9128287d7c3f12691c8
H8-Dev <h8.pr0j1@gmail.com>
2026-09-14 18:58:58 -0300

    Updated editor files to match current testing

--- frontend/apm/editor.js
+++ frontend/apm/editor.js
@@ -1,22 +1,94 @@
 const API_URL = "http://127.0.0.1:5000/arquivos";
 
-const arquivo = document.getElementById("area_arquivo");
+const arquivo = document.getElementById("text-editor");
+const abaLateral = document.getElementById("right-panel");
+const nomeArq = document.getElementById("file-name");
+const salvar = document.getElementById("saveBtn");
 
+let arqAtivo = null;
 
 async function fetchDocumentos() {
+    pac_id = localStorage.getItem('pac_id');
+    med_id = localStorage.getItem('med_id');
+    const dados = {
+        pac_id: pac_id,
+        med_id: med_id
+    };
     try {
-        const response = await fetch(`${API_URL}/${encodeURIComponent(pac_id)}`);
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dados)
+        });
+
         const documentos = await response.json();
-        return documentos;
+
+        documentos.forEach(doc => {
+            const novoDoc = document.createElement("div");
+            novoDoc.className = "panel-box";
+            novoDoc.textContent = `📄 ${doc.nome_arq}`;
+
+            novoDoc.addEventListener("click", () => {
+               document.querySelectorAll('.panel-box').forEach(box => box.classList.remove('active'));
+                novoDoc.classList.add('active');
+
+                mudarDocumento(doc.arq_id, doc.nome_arq);
+            });
+        })
     } catch (error) {
         console.error("Erro ao buscar documentos:", error);
     }
 }
 
-function mudarDocumento(idDoc) {
-  
-  arquivo.value = documentos[id];
+async function mudarDocumento(id, nome) {
+    try {
+        nomeArq.value = nome;
+
+        const response = await fetch(`${API_URL}/${id}`);7
+
+        const textoArq = await response.text();
+        arqAtivo = id;
+        arquivo.value = textoArq;
+        
+    } catch (error) {
+        console.error("Erro ao buscar documento:", error);
+        arquivo.value = `Erro ao carregar o documento: ${error.message}`;
+    }
 }
+
+salvar.addEventListener("click", async () => {
+    if (!arqAtivo) return;
+
+    const dados = {
+        arq_id: arqAtivo,
+        nome_arq: nomeArq.value,
+        dados: arquivo.value
+    };
+
+    try {
+        const response = await fetch(`${API_URL}/${arqAtivo}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dados)
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+            alert("Documento salvo com sucesso!");
+        } else{
+            alert(`Erro ao salvar documento: ${result.error}`);
+        }
+
+
+    } catch (error) {
+        console.error("Erro ao salvar documento:", error);
+    }
+
+});
 
 
 function ferramentaDeBusca(){
@@ -50,3 +122,8 @@ function toggleModal(show) {
         modal.classList.remove('active');
       }
     }
+
+
+window.addEventListener('DOMContentLoaded', function() {
+    fetchDocumentos();
+});
